4783c72a9ab980be4b5260818c159d1c1f481122
mahadi24t <mahadi24t@outlook.com>
2025-07-31 17:13:44 +0600

    some small changes

--- src/App.jsx
+++ src/App.jsx
@@ -39,15 +39,49 @@ import { createRoot } from 'react-dom/client'
 const root = createRoot(document.querySelector("#root"))
 
 root.render(
-  <main>
-  <img src="src\assets\react-logo.png" width="40px" alt="React logo"></img>
-  <h1>Fun facts about React!</h1>
-        <ul>
-            <li>Was first release in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 200K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-  </main>
+  <>
+    <MainPage />
+    <Page />
+    <FooterPage />
+  </>
 )
+
+function MainPage() {
+  return (
+    <div><header><img src="src/assets/react-logo.png" width="40px" alt="React logo" /></header>
+    <main>
+      <h1>Fun facts about React!</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <section>
+      <h1>Why I am excited for Learning React!</h1>
+      <ol>
+        <li>React Powers Real-World Web Apps</li>
+        <li>Component-Based Thinking Mirrors Cognitive Modularity</li>
+        <li>React Ecosystem = Toolbox for Everything</li>
+        <li>React Makes Projects Globally Accessible</li>
+        <li>React Develops Intuitive Problem Solving</li>
+      </ol>
+    </section>
+  )
+}
+
+function FooterPage(){
+  return(
+    <footer><small>
+      © 2025 developed by Mahadi Hasan. All rights reserved.
+      </small></footer>
+  )
+}
